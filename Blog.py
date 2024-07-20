50bef08535fc1b33741f831e6285330a2adde3d4
bacuna153 <bacuna153@gmail.com>
2024-07-20 12:03:23 +0100

    the second comit

---
+++ Blog.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+Posts = [
+    {
+        'author':'Idris Obaka',
+        'title': 'Blog Post 1',
+        'content':'First post content',
+        'date_posted': 'July 20 2024'
+    },
+        {
+        'author':'Ibrahim Obaka',
+        'title': 'Blog Post 2',
+        'content':'Second post content',
+        'date_posted': 'July 25 2024'
+    }
+]
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html',posts=Posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+if __name__== '__main__':
+    app.run(debug=True)
+
