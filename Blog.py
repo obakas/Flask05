6aef8520e6cf84a401d0eb26bd9887c4feef4c5c
bacuna153 <bacuna153@gmail.com>
2024-07-20 12:51:27 +0100

    my third commit

--- Blog.py
+++ Blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
@@ -24,7 +24,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='about')
 
 
 if __name__== '__main__':
